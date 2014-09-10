2a5400a7f35c05f9de121cc3ba7bcab263a51215
Aaron Craelius <aaroncraelius@gmail.com>
2014-09-10 10:32:53 -0400

    Updated version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fx-clj "0.1.0-SNAPSHOT"
+(defproject fx-clj "0.2.0-SNAPSHOT"
   :description "A Clojure library for JavaFX"
   :url "https://github.com/aaronc/fx-clj"
   :license {:name "Eclipse Public License"
